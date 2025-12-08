d2775c7ad207dba0a0ab9e7403d6f9394bf3891e
Conducta <kyle12as@gmail.com>
2025-12-08 09:07:28 +0800

    Update tempCodeRunnerFile.py

--- person/tempCodeRunnerFile.py
+++ person/tempCodeRunnerFile.py
@@ -1,2 +1,2 @@
-        self.tree.tag_configure('borrowed', background='red')
-        self.tree.tag_configure('returned', background='green')
+self.tree.tag_configure('borrowed', background='red')
+self.tree.tag_configure('returned', background='green')
